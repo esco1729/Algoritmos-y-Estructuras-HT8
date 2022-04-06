1dd6b458929057c404eaaf14acb55b6053f75212
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-04-06 09:09:59 -0600

    Updata Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
-
+	public static void main(String args[]) throws FileNotFoundException{
+		File file = new File("datos.txt");
+	}
 }
