c86a99242062d7e96e65f74f1b97cac249604eeb
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-04-07 21:57:18 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //http://www.java2s.com/Code/Java/File-Input-Output/Readeachlineinacommaseparatedfileintoanarray.htm
 public class Main {
-
-	static PriorityQueue<Paciente> priorityQueue;
-	
+	public static void main(String[] args) throws FileNotFoundException {
+		Vista vista = new Vista();
+		vista.data();
+	}
 }
 
-	
 
         
         
--- Vista.java
+++ Vista.java
@@ -1,7 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Vista {
-
-	
+	  public ArrayList<String> data() throws FileNotFoundException{
+		  Scanner s = new Scanner(new File("C:\\Users\\liter\\OneDrive\\Escritorio\\Universidad\\Semestre V\\Algoritmos y Estructuras de Datos\\Laboratorio 8\\Algoritmos-y-Estructuras-HT8\\pacientes"));
+		  ArrayList<String> list = new ArrayList<String>();
+		  while (s.hasNext()) {
+			  list.add(s.next());
+		  }
+		  for(String str: list) {
+			  System.out.println(str);
+		  }
+	  s.close();
+	return list;
+	  }
+	  
 }
