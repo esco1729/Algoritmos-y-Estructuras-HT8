def6759cdb665cbb6edbfb6655585c3ecf1d67b8
escobar1729 <61253307+escobar1729@users.noreply.github.com>
2022-04-08 19:56:22 -0600

    Update Vista.java

--- Main.java
+++ Main.java
@@ -10,8 +10,42 @@ import java.util.Scanner;
 //http://www.java2s.com/Code/Java/File-Input-Output/Readeachlineinacommaseparatedfileintoanarray.htm
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
-		Vista vista = new Vista();
-		vista.data();
+		boolean seguir = true;
+		Vista vis = new Vista();
+		Scanner scn = new Scanner(System.in);
+		VectorHeap<Paciente> hospital = new VectorHeap<>();
+		
+		while(seguir) {
+			System.out.println("\nLocation (ubicación del archivo con los pacientes): ");
+			String path = scn.nextLine();
+			vis.data(path);
+			
+			do {
+				System.out.println("1. Ver pacientes (método 1)");
+				System.out.println("2. Ver pacientes (método 2)");
+				String opc = scn.nextLine();
+				
+				if(opc == "1") {
+					
+				}
+				
+				else if (opc == "2") {
+					
+				}
+				
+				else {
+					break;
+				}
+				
+				
+				
+				
+			}while(seguir);
+			
+			
+			
+			
+		}
 	}
 }
 
--- Vista.java
+++ Vista.java
@@ -1,37 +1,55 @@
+import java.awt.List;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Vista {
-		public String path() {
+	
+		public static String path() {
 			Scanner scn = new Scanner(System.in);
-			System.out.println("\nLocation (ubicación del archivo): ");
-			return scn.nextLine();
+			System.out.println("\nLocation (ubicación del archivo con los pacientes): ");
+			String path = scn.nextLine();
+			return path;
 		}
 	
-	  public ArrayList<String> data() throws FileNotFoundException{
-		  String myPath = path();
-		  Scanner s = new Scanner(new File(myPath));
-		  ArrayList<String> list = new ArrayList<String>();
-		  while (s.hasNext()) {
-			  list.add(s.next());
-		  }
-		  for(String str: list) {
-			  System.out.println(str);
-		  }
-	  s.close();
-	return list;
-	  }
-	  
-	  public PriorityQueue makePriority() {
-		  return null;
-		  
-	  }
-	  
-	  public VectorHeap makeVector() {
-		return null;
-		  
-	  }
-}
+	  public void data(String path) throws FileNotFoundException{
+		  VectorHeap<Paciente> pacientesVector = new VectorHeap<>();
+		    BufferedReader br = null;
+		    String[] characters = new String[1024];
+
+		    try {
+
+		        String sCurrentLine;
+		        br = new BufferedReader(new FileReader(path));
+
+		        int i=0;
+		        while ((sCurrentLine = br.readLine()) != null) {
+		            String[] arr = sCurrentLine.split(",");
+		            System.out.println("arr[0] = " + arr[0]); // nombre
+		            System.out.println("arr[1] = " + arr[1]); // sintoma
+		            System.out.println("arr[2] = " + arr[2]); // codigo
+
+
+		            characters[0] = arr[0];
+		            characters[1] = arr[1];
+		            characters[2] = arr[2];
+		            Paciente paci = new Paciente(characters[0], characters[1], characters[2]);
+		            pacientesVector.add(paci);
+		            i++;
+		        }
+
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    } finally {
+		        try {
+		            if (br != null)br.close();
+		        } catch (IOException ex) {
+		            ex.printStackTrace();
+		        }
+		    }
+	  }}
